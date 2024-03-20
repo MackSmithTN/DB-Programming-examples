18cb3bdf95929d234b882417ad4e736c93a5ab8c
Susan Hammond <sahammond@lipscomb.edu>
2024-03-20 10:39:26 -0500

    Fixed hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,14 @@ from flask import Flask
 app = Flask(__name__) # Flask constructor 
 
 # A decorator used to tell the application 
-# which URL is associated function 
+# which URL is associated with what function 
 @app.route('/hello')	 
 def hello(): 
 	return 'HELLO'
+
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run(port=8000, debug=True)
